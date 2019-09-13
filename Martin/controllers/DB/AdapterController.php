a3e49f8baaeebb6c2dc7abdabff6b7317af076d8
lei1314lei <547249121@qq.com>
2019-09-13 10:06:28 +0800

    adjust structor

---
+++ Martin/controllers/DB/AdapterController.php
@@ -0,0 +1,34 @@
+<?php
+
+
+class Martin_Debug_DB_AdapterController extends Mage_Core_Controller_Front_Action{
+    public function indexAction()
+    {
+        echo 'ok';
+    }
+}
+
+
+
+
+//    const FETCH_ASSOC = 2;
+//    const FETCH_BOTH = 4;
+//    const FETCH_BOUND = 6;
+//    const FETCH_CLASS = 8;
+//    const FETCH_CLASSTYPE = 262144;
+//    const FETCH_COLUMN = 7;
+//    const FETCH_FUNC = 10;
+//    const FETCH_GROUP = 65536;
+//    const FETCH_INTO = 9;
+//    const FETCH_LAZY = 1;
+//    const FETCH_NAMED = 11;
+//    const FETCH_NUM = 3;
+//    const FETCH_OBJ = 5;
+//    const FETCH_ORI_ABS = 4;
+//    const FETCH_ORI_FIRST = 2;
+//    const FETCH_ORI_LAST = 3;
+//    const FETCH_ORI_NEXT = 0;
+//    const FETCH_ORI_PRIOR = 1;
+//    const FETCH_ORI_REL = 5;
+//    const FETCH_SERIALIZE = 524288;
+//    const FETCH_UNIQUE = 196608;
